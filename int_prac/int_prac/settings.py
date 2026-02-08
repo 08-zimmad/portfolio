8df658606f2e3c095e7b3bbb397ca0e64172e660
zimmadw <zimmad.w@gmail.com>
2026-02-08 17:41:41 +0500

    Reddit conf

--- int_prac/int_prac/settings.py
+++ int_prac/int_prac/settings.py
@@ -182,3 +182,20 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
+
+
+# Redis Setup
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# session caches instead of db, in reddis
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
