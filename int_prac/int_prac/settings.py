0a10f7cdb2831465483303885825a329be4fc039
zimmadw <zimmad.w@gmail.com>
2026-04-06 15:25:46 +0500

    fixed missing authentication_backend

--- int_prac/int_prac/settings.py
+++ int_prac/int_prac/settings.py
@@ -118,6 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTHENTICATION_BACKENDS = (
     "social_core.backends.google.GoogleOAuth2",  # example
+    "django.contrib.auth.backends.ModelBackend",
 )
 
 # Internationalization
